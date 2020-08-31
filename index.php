6f504a73d23ce588a33df28deac6a175641de9b8
PeterStollenwerk <ps@peterstollenwerk.de>
2020-08-31 18:23:48 +0200

    removed autoload setup

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 # set them in the composer json
 # to make the classes work with php unit!
 
-@include_once __DIR__.'/vendor/autoload.php'; # all classes set in composer.json > psr-4 are loaded here!
+// @include_once __DIR__.'/vendor/autoload.php'; # all classes set in composer.json > psr-4 are loaded here!
 
-// load([
-//   'auf\\plugin\\testclass' => 'lib/TestClass.php'
-// ], __DIR__);
+load([
+  'auf\\MegaSuperClass' => 'lib/MegaSuperClass.php'
+], __DIR__);
 
 Kirby::plugin('auf/plugin', [
     'fields' => [
--- site/templates/default.php
+++ site/templates/default.php
@@ -1,2 +1,14 @@
 <h1><?= $page->title() ?></h1>
 <?= $page->aufpluginfield() ?>
+
+
+
+<?php
+
+use auf\MegaSuperClass;
+
+$mega = new MegaSuperClass('test');
+
+echo $mega->test;
+
+?>
